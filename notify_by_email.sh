9ac38363eedf34faaf1fd213b5d733903808565b
M Starch <Michael.D.Starch@jpl.nasa.gov>
2017-01-10 22:21:46 +0000

    lightweight hello world

---
+++ notify_by_email.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source $HOME/verdi/bin/activate
+
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+
+# check args
+if [ "$#" -eq 4 ]; then
+  id=$1
+  url=$2
+  emails=$3
+  rule_name=$4
+else
+  echo "Invalid number or arguments ($#) $*" 1>&2
+  exit 1
+fi
+
+# send email
+echo "##########################################" 1>&2
+echo -n "Sending email: " 1>&2
+date 1>&2
+python $BASE_PATH/notify_by_email.py "$id" "$url" "$emails" "$rule_name" > notify_by_email.log 2>&1
+STATUS=$?
+echo -n "Finished sending email: " 1>&2
+date 1>&2
+if [ $STATUS -ne 0 ]; then
+  echo "Failed to send email." 1>&2
+  cat notify_by_email.log 1>&2
+  echo "{}"
+  exit $STATUS
+fi
+
+exit 0
