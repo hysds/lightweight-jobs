c275bdb576ecde1cc266ce82995439fffd0b6d4c
N Malarout <namrata.malarout@jpl.nasa.gov>
2017-02-01 22:30:19 +0000

    malarout: added purge and revoke for mozart

--- purge.py
+++ purge.py
@@ -11,7 +11,7 @@ from hysds.celery import app
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("hysds")
 
-def purge_products(query):
+def purge_products(query,component,operation):
     '''
     Iterator used to iterate across a query result and submit jobs for every hit
     @param es_url - ElasticSearch URL to hit with query
@@ -20,9 +20,16 @@ def purge_products(query):
     @param query - query to post to ElasticSearch and whose result will be iterated, JSON sting enc
     @param kwargs - key-word args to match to HySDS IO
     '''
+    logger.debug("Doing %s for %s with query: %s",operation,component,query)
     
-    es_url = app.conf["GRQ_ES_URL"]
-    es_index = app.conf["DATASET_ALIAS"]
+    if component=="mozart":
+        es_url = app.conf["JOB_ES_URL"]
+        query_idx = app.conf["STATUS_ALIAS"]
+        facetview_url = app.conf["MOZART_URL"]
+    elif component=="tosca":
+        es_url = app.conf["GRQ_ES_URL"]
+        query_idx = app.conf["DATASET_ALIAS"]
+        facetview_url = app.conf["GRQ_URL"]
 
     #Querying for products
     start_url = "{0}/{1}/_search".format(es_url,es_index)
@@ -31,22 +38,63 @@ def purge_products(query):
     results = hysds_commons.request_utils.post_scrolled_json_responses(start_url,scroll_url,data=json.dumps(query),logger=logger)
     print results
     
-    for result in results:
-    	es_type = result["_type"]
-    	ident = result["_id"]
-	index = result["_index"]
-	#find the Best URL first
-	best = None
-	for url in result["_source"]["urls"]:
-		if best is None or not url.startswith("http"):
-			best = url
+    if component is 'tosca':
+    	for result in results:
+    		es_type = result["_type"]
+    		ident = result["_id"]
+		index = result["_index"]
+		#find the Best URL first
+		best = None
+		for url in result["_source"]["urls"]:
+			if best is None or not url.startswith("http"):
+				best = url
     
-   	#making osaka call to delete product
-	print 'paramater being passed to osaka.main.rmall: ',best
-	osaka.main.rmall(best)
-	#removing the metadata
-	hysds_commons.metadata_rest_utils.remove_metadata(es_url,index,es_type,ident,logger)
+   		#making osaka call to delete product
+		print 'paramater being passed to osaka.main.rmall: ',best
+		osaka.main.rmall(best)
+		#removing the metadata
+		hysds_commons.metadata_rest_utils.remove_metadata(es_url,index,es_type,ident,logger)
 
+    elif component is 'mozart':
+	if operation is 'purge':
+		purge = True
+	else:
+		purge = False
+	# purge job from index
+    
+        for result in results:
+            uuid = result["_source"]['uuid']
+            payload_id = result["_source"]['payload_id']
+	    index = result["_index"]
+            es_type = result['_type']
+            #Always grab latest state (not state from query result)
+            task = app.AsyncResult(uuid)
+            state = task.state
+            #Active states may only revoke
+            logger.info("Job state: %s\n",state)
+            if state in ["RETRY","STARTED"] or (state == "PENDING" and not purge):
+                if not purge:
+                    logger.info('Revoking %s\n',uuid)
+                    app.control.revoke(uuid,terminate=True)
+                else:
+                    logger.info( 'Cannot remove active job %s\n',uuid)
+                continue
+            elif not purge:
+                logger.info( 'Cannot stop inactive job: %s\n',uuid)
+                continue
+            #Saftey net to revoke job if in PENDING state
+            if state == "PENDING":
+                logger.info( 'Revoking %s\n',uuid)
+                app.control.revoke(uuid,terminate=True)
+            
+            # Both associated task and job from ES
+            logger.info( 'Removing ES for %s:%s',es_type,payload_id)
+	    r = hysds_commons.metadata_rest_utils.remove_metadata(es_url,index,es_type,uuid,logger) 
+            #r.raise_for_status() #not req
+            #res = r.json() #not req
+            logger.info('done.\n')
+        logger.info('Finished\n')
+    
 
 if __name__ == "__main__":
     '''
@@ -54,12 +102,13 @@ if __name__ == "__main__":
     '''
     #encoding to a JSON object
     query_obj = {}
-    #dec = sys.argv[1].replace("u'\"'\"'","'")
-    #decoded_inp = dec.replace("'\"'\"'","'")
     decoded_string = sys.argv[1].decode('string_escape')
     dec = decoded_string.replace('u""','"')
     decoded_inp = dec.replace('""','"')
     print(decoded_inp)
     query_obj['query'] = json.loads(decoded_inp)
-    purge_products(query_obj)
+
+    component = sys.argv[2]
+    operation = sys.argv[3]
+    purge_products(query_obj,component,operation)
 
--- purge.sh
+++ purge.sh
@@ -5,9 +5,10 @@ source $HOME/verdi/bin/activate
 BASE_PATH=$(dirname "${BASH_SOURCE}")
 
 # check args
-if [ "$#" -eq 2 ]; then
+if [ "$#" -eq 3 ]; then
   query=$1
   component=$2
+  operation=$3
 else
   echo "Invalid number or arguments ($#) $*" 1>&2
   exit 1
@@ -15,14 +16,14 @@ fi
 
 # purge products
 echo "##########################################" 1>&2
-echo -n "Purging products: " 1>&2
+echo -n "Purge/Stop/Revoke products: " 1>&2
 date 1>&2
-python $BASE_PATH/purge.py "$query" "$component" > purge.log 2>&1
+python $BASE_PATH/purge.py "$query" "$component" "$operation" > purge.log 2>&1
 STATUS=$?
-echo -n "Finished purging products: " 1>&2
+echo -n "Finished purging/revoking: " 1>&2
 date 1>&2
 if [ $STATUS -ne 0 ]; then
-  echo "Failed to purge products." 1>&2
+  echo "Failed to purge/revoke." 1>&2
   cat purge.log 1>&2
   echo "{}"
   exit $STATUS
