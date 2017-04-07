7dfcfc19e12eb891904cc815423546328232890a
Namrata Malarout <Namrata.Malarout@jpl.nasa.gov>
2017-04-06 20:30:17 -0700

    fix for decoded passthrough

--- purge.py
+++ purge.py
@@ -102,10 +102,11 @@ if __name__ == "__main__":
     '''
     #encoding to a JSON object
     query_obj = {}
-    decoded_string = sys.argv[1].decode('string_escape')
-    dec = decoded_string.replace('u""','"')
-    decoded_inp = dec.replace('""','"')
-    print(decoded_inp)
+    #decoded_string = sys.argv[1].decode('string_escape')
+    #dec = decoded_string.replace('u""','"')
+    #decoded_inp = dec.replace('""','"')
+    decoded_inp = sys.argv[1]
+    print decoded_inp
     query_obj["query"] = json.loads(decoded_inp)
 
     component = sys.argv[2]
