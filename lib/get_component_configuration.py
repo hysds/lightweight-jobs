51adf2925d93f87265f8cfd238c6ea8b545138ba
Gerald Manipon <pymonger@gmail.com>
2019-02-14 07:58:31 -0800

    port to python3 (#4)
    * initial 2to3 run
    
    * format to PEP8 spec

--- lib/get_component_configuration.py
+++ lib/get_component_configuration.py
@@ -1,15 +1,16 @@
 from hysds.celery import app
 
+
 def get_component_config(component):
     '''
     From a component get the common configuration values
     @param component - component 
     '''
-    if component=="mozart" or component=="figaro":
+    if component == "mozart" or component == "figaro":
         es_url = app.conf["JOBS_ES_URL"]
         query_idx = app.conf["STATUS_ALIAS"]
         facetview_url = app.conf["MOZART_URL"]
-    elif component=="tosca":
+    elif component == "tosca":
         es_url = app.conf["GRQ_ES_URL"]
         query_idx = app.conf["DATASET_ALIAS"]
         facetview_url = app.conf["GRQ_URL"]
