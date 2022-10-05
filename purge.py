be81e895e3b747d44ab65aa14f34f43c3e06fa93
andrewmz <andrewmz@jpl.nasa.gov>
2022-10-05 11:56:15 -0700

    SSDS-2703:test

--- purge.py
+++ purge.py
@@ -39,8 +39,8 @@ def delete_dataset(es, es_result, deleted_datasets):
             best = url
 
     print('paramater being passed to osaka.main.rmall: ', best)  # making osaka call to delete product
-    if best is not None:
-        osaka.main.rmall(best)
+    #if best is not None:
+        #osaka.main.rmall(best)
 
     tosca_es.delete_by_id(index=index, id=ident, ignore=404)  # removing the metadata
     logger.info('Purged %s' % ident)
