13f49b2ea21d449de14ccf82c5c2cd8a7b41df49
andrewmz <andrewmz@jpl.nasa.gov>
2022-10-05 23:04:57 -0700

    SSDS-2703:test

--- purge.py
+++ purge.py
@@ -72,7 +72,7 @@ def purge_products(query, component, operation):
         deleted_datasets = dict()
         updated_deletion = [p.apply_async(delete_dataset, args=(results, deleted_datasets)) for i in
                             range(0, num_processes)]
-        deleted_datasets = updated_deletion.get()
+        print(updated_deletion)
         if len(deleted_datasets) != 0:
             msg_details = "Datasets purged by type:\n\n"
             for ds in deleted_datasets.keys():
