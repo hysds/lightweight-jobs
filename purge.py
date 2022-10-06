fec8239c78f92a5d049b99867346dfca66fc41f4
andrewmz <andrewmz@jpl.nasa.gov>
2022-10-06 01:21:49 -0700

    SSDS-2703:test

--- purge.py
+++ purge.py
@@ -70,11 +70,7 @@ def purge_products(query, component, operation):
     p = Pool(processes=num_processes)
     if component == 'tosca':
         deleted_datasets = dict()
-        updated_deletion = [p.apply_async(delete_dataset, args=(results, deleted_datasets)) for i in
-                            range(0, num_processes)]
-        p.close()
-        p.join()
-        print(updated_deletion)
+        deleted_datasets = p.starmap(delete_dataset, (results, deleted_datasets))
         if len(deleted_datasets) != 0:
             msg_details = "Datasets purged by type:\n\n"
             for ds in deleted_datasets.keys():
