7bb61051f17de8b801902c8a1fb6692bfadaf939
dustinlo <dustin.k.lo@jpl.nasa.gov>
2023-10-17 14:25:36 -0700

    removed resource

--- retry.py
+++ retry.py
@@ -169,7 +169,7 @@ def resubmit_jobs(context):
 
 if __name__ == "__main__":
     ctx = read_context()
-    input_type = ctx['resource']
+    # input_type = ctx['resource']
     # if input_type == "job":
     resubmit_jobs(ctx)
     # else:
